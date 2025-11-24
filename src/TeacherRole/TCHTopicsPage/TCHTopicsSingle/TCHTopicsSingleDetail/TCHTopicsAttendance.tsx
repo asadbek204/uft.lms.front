1eb389eeedf928295002a8cabc123fe73a4122c3
Asadbek <ahikmatullayev024@gmail.com>
2025-11-24 12:44:10 +0500

    [fix] qr attendance && and no mobile mode

--- src/TeacherRole/TCHTopicsPage/TCHTopicsSingle/TCHTopicsSingleDetail/TCHTopicsAttendance.tsx
+++ src/TeacherRole/TCHTopicsPage/TCHTopicsSingle/TCHTopicsSingleDetail/TCHTopicsAttendance.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import client, { APIURL } from "../../../../components/services"
 import { useParams } from 'react-router-dom'
 
